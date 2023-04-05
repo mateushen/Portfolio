761f078b16ea380ba6e89d5024ace22cef07a4f5
Mateus <mateus.hen65@gmail.com>
2023-04-04 22:44:53 -0300

    criação de novos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import './App.scss'
 import Header from './components/header';
 import Footer from './components/footer';
+import Summary from './components/summary';
 
 function App() {
 
+  const [step, setStep] = useState(1);
+
+  function buttonClick(){
+    setStep(step+1);
+  }
+
   return (
-    <div className="App">
+    <div>
       <Header />
 
-      <div className="main">
-        
-      </div>
+      <main className="main">
+        <Summary />
+      </main>
 
       <Footer />
     </div>
---
+++ src/components/summary/index.jsx
@@ -0,0 +1,20 @@
+import './summary.scss'
+import foto from '../../global/img/eu.png'
+
+export default function Summary() {
+    return (
+        <div className="main">
+            <div className="container">
+                <img className="img" src={foto} />
+                <div className="resume">
+                    <span>
+                        testeteteudshughdfudfhdgjggfbffffffffffffffffffffffffffffffffffffffffff
+                        gffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+                        gfdhgfhbgfbfgbgfbdbnndffffffgggggggggggggggggggggggggggggggg
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
